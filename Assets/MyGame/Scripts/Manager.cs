b2e41be544233609819f250e2932b21521f74146
lforsthuber05 <laura.forsthuber05@htl-salzburg.ac.at>
2022-11-23 21:11:35 +0100

    init prj

---
+++ Assets/MyGame/Scripts/Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    /*
+     * man kann nicht ¸ber die Startlinie
+     * Spiel beginnt wenn man Startlinie betritt
+     * pro Versuch kann nur ein Baum genommen werden
+     * messen der Wurfweite
+     * sehen ob die Zielscheibe getroffe wurde
+     * Physics f¸r B‰ume
+     * Toneffekte: ->  Baumaufprall (Boden und Zielscheibe), 
+     *             ->  Weihnachtslied f¸r Hintergrund
+     *             ->  Ausreiﬂen der B‰ume
+     * 
+     * Enum: weiﬂ noch nicht
+     * Methodendeklaration/-aufruf: Braucht man immer
+     * If-Statement: z.B. hat der Baum die Zielscheibe getroffen ja/nein
+     */
+}
